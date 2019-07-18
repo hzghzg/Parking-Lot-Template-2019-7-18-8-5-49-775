de7915f0d9cb51358d17167728667bc1fafa674c
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-18 21:59:47 +0800

    Feat:add a method addParkingLot

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -1,4 +1,28 @@
 package com.thoughtworks.parking_lot.controller;
 
+import com.thoughtworks.parking_lot.entity.ParkingLot;
+import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parkinglots")
 public class ParkingLotController {
+    @Autowired
+    ParkingLotRepository parkingLotRepository;
+    List<ParkingLot> parkingLotList=new ArrayList<>();
+    //story AC1
+    @PostMapping
+    public List<ParkingLot> addParkingLot(@RequestBody ParkingLot parkingLot){
+        parkingLotList.add(parkingLot);
+        parkingLotRepository.save(parkingLot);
+        parkingLotList=parkingLotRepository.findAll();
+        return parkingLotList;
+    }
 }
--- src/main/java/com/thoughtworks/parking_lot/repository/ParkingLotRepository.java
+++ src/main/java/com/thoughtworks/parking_lot/repository/ParkingLotRepository.java
@@ -2,6 +2,7 @@ package com.thoughtworks.parking_lot.repository;
 
 import com.thoughtworks.parking_lot.entity.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 public interface ParkingLotRepository extends JpaRepository<ParkingLot,String> {
 
